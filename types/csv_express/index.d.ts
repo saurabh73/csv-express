9559947c8df9a7c865c12d72440c558ae6dc689d
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2018-05-29 07:19:50 +0530

    Typd Defintion and Middlware

---
+++ types/csv_express/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for csv_express x.x
+// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function csv_express(req: any, res: any, next: any): any;
+
+
+declare namespace csv_express {
+    const prototype: {
+    };
+
+}
+
